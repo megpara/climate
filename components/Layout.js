3d5e28ae48f1997e25ab9dc806ac5d65c2af0027
Megan Paradowski <meganparadowski@Megans-MacBook-Pro.local>
2021-11-12 15:13:00 -0800

    Initial styling and background

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
+import styles from "../styles/Layout.module.css";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <title>The West Coast Climate Crisis Symposium</title>
         <meta
@@ -10,6 +11,13 @@ export default function Layout({ children }) {
           content="The West Coast Climate Crisis Symposium"
         />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://use.typekit.net/qfx7urw.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       {children}
     </div>
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,18 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <Layout>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.titleDiv}>
+          <div className={styles.title}>
+            The West Coast Climate Crisis Symposium
+          </div>
+          <div className={styles.subtitle}>
+            The needed role of science journalists in effecting faster and more
+            positive change in stopping the climate crisis
+          </div>
+          <div className={styles.introText}></div>
+        </div>
+      </div>
     </Layout>
   );
 }
