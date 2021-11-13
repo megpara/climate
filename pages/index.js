939de69bd8711eadb21e6cebe2d6a02a58633a18
Megan Paradowski <meganparadowski@Megans-MacBook-Pro.local>
2021-11-12 17:01:03 -0800

    Styling

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import Layout from "../components/Layout";
+import Layout from "../components/Layout.js";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout>
+      <div className={styles.backgroundImage} />
       <div className={styles.container}>
+        <div className={styles.eventDetails}>
+          <div className={styles.detail}>Los Angeles, UCLA</div>
+          <div className={`${styles.detail} ${styles.detailCenter}`}>
+            March 19, 2022
+          </div>
+          <div className={`${styles.detail} ${styles.detailRight}`}>
+            9am - 6pm
+          </div>
+        </div>
         <div className={styles.titleDiv}>
           <div className={styles.title}>
             The West Coast Climate Crisis Symposium
@@ -13,7 +23,10 @@ export default function Home() {
             The needed role of science journalists in effecting faster and more
             positive change in stopping the climate crisis
           </div>
-          <div className={styles.introText}></div>
+          <div className={styles.emailTitle}>Sign up for our mailing list</div>
+          <input className={styles.emailInput} />
+          <button className={styles.mainButton}>Submit</button>
+          {/* <button className={styles.mainButton}>View the lineup</button> */}
         </div>
       </div>
     </Layout>
