2da085c3202728255c2d939114db8654502c7d5b
Megan Paradowski <meganparadowski@Megans-MacBook-Pro.local>
2021-12-01 16:07:46 -0800

    Favicon

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
           name="description"
           content="The West Coast Climate Crisis Symposium"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/square_logo.png" />
         <link rel="stylesheet" href="https://use.typekit.net/qfx7urw.css" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,900&display=swap"
