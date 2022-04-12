a2ebc9777ad22f3f5e849c09f91762943ab342db
aklevecz <teh@raptor.pizza>
2022-04-11 21:57:07 -0700

    starting donation

---
+++ constants.js
@@ -0,0 +1,16 @@
+export const donationButtonHtml = `<div id="donate-button-container">
+<div id="donate-button"></div>
+<script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
+<script>
+PayPal.Donation.Button({
+env:'production',
+hosted_button_id:'YRHG4VGPBSTDU',
+image: {
+src:'https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif',
+alt:'Donate with PayPal button',
+title:'PayPal - The safer, easier way to pay online!',
+}
+}).render('#donate-button');
+</script>
+</div>
+`;
