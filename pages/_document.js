d0119650cffe66a2e83e5b9396c9cd545f30bdc6
aklevecz <teh@raptor.pizza>
2022-04-16 13:51:10 -0700

    og image

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/square_logo.png" />
+        <meta
+          property="og:image"
+          content="https://westcoastclimatecrisis.org/og-image.png"
+        />
         <link rel="stylesheet" href="https://use.typekit.net/qfx7urw.css" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,900&display=swap"
