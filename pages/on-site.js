cb3c6e92f4036b5e7fd8e7a9a635f26bda1ab368
aklevecz <teh@raptor.pizza>
2022-06-01 12:25:17 -0700

    force update

--- pages/on-site.js
+++ pages/on-site.js
@@ -1,4 +1,17 @@
 export default function OnSite() {
+  // 1. A Page and a link to a UCLA campus map, with Ackerman Hall highlighted, and parking lots 4 and 7 highlighted.
+
+  // 2. We need another page for additional instructions:
+
+  // Parking: Lots 4 & 7 (see map).
+
+  // Food Court for Lunch:  Carl's Junior; Rubio's; Panda Express; Veggie Grill.
+
+  // You may also bring your lunch.
+
+  // There are also buses and a metro stop in Westwood.
+
+  // We may need to add a page for Barney's Beanery. I'm checking right now.
   return (
     <div>
       <div>map</div>
